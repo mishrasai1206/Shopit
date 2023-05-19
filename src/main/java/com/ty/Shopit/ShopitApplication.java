1bdd3c4fa901919801059e953a6e0fcc2a1612b9
mishrasai1206 <mishrasai1206@gmail.com>
2023-05-19 10:18:40 +0530

    Project Created and first commit to master

---
+++ src/main/java/com/ty/Shopit/ShopitApplication.java
@@ -0,0 +1,13 @@
+package com.ty.Shopit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopitApplication.class, args);
+	}
+
+}
